6c80ec27b784c3946f376b119cb7020401c6057c
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-03-05 00:06:05 -0500

    Refactored code

--- NN/Main.py
+++ NN/Main.py
@@ -1,3 +1,5 @@
+# Neural Network with one hidden layer
+
 import numpy as np
 from scipy.optimize import *
 import csv
@@ -9,11 +11,20 @@ def loadCSV (filePath, datatype = np.int) :
     return np.asarray(data, dtype=datatype)
 
 
-data = loadCSV('../train.csv')
+train = loadCSV('../train.csv')
 test = loadCSV('../test.csv')
-reg = 3.0
-hiddenLayerSize = 50
-Nfeval = 1
+
+featureSize = 785
+hiddenSize = 100
+outputSize = 10
+
+dataSize = 42000
+testSize = 28000
+
+reg = 5.0
+
+maxIterations = 200
+iteration = 1
 
 def sigmoid (x) :
     return 1.0 / (1 + np.exp(-x))
@@ -24,97 +35,103 @@ def sigmoidGrad (x) :
 # args = (data)
 # x: (785 x 29, 30 x 10)
 
-def predict (x, m) :
-    hiddenLayerTheta = x[0 : 785 * (hiddenLayerSize - 1)].reshape((785, (hiddenLayerSize - 1)))
-    outputLayerTheta = x[785 * (hiddenLayerSize - 1) : 785 * (hiddenLayerSize - 1) + hiddenLayerSize * 10].reshape((hiddenLayerSize, 10))
+def predict (x, data, m, featureSize, hiddenSize, outputSize) :
+    hiddenTheta = x[0 : featureSize * (hiddenSize - 1)].reshape((featureSize, (hiddenSize - 1)))
+    outputTheta = x[featureSize * (hiddenSize - 1) : featureSize * (hiddenSize - 1) + hiddenSize * outputSize].reshape((hiddenSize, outputSize))
     
-    # 42000 x 785
-    features = np.ones((m, 785))
-    features[:, 1:] = test / 255.0
+    # m x featureSize
+    features = np.ones((m, featureSize))
+    features[:, 1:] = data / 255.0
 
-    # 42000 x 30
-    hiddenLayer = np.ones((m, hiddenLayerSize))
-    hiddenLayer[:, 1:] = sigmoid(features.dot(hiddenLayerTheta))
+    # m x hiddenLayerSize
+    hiddenLayer = np.ones((m, hiddenSize))
+    hiddenLayer[:, 1:] = sigmoid(features.dot(hiddenTheta))
     
-    # 42000 x 10
-    outputLayer = sigmoid(hiddenLayer.dot(outputLayerTheta))
+    # m x 
+    outputLayer = sigmoid(hiddenLayer.dot(outputTheta))
     
     return np.argmax(outputLayer, 1)
 
-def getCost (x):
-    hiddenLayerTheta = x[0 : 785 * (hiddenLayerSize - 1)].reshape((785, (hiddenLayerSize - 1)))
-    outputLayerTheta = x[785 * (hiddenLayerSize - 1) : 785 * (hiddenLayerSize - 1) + hiddenLayerSize * 10].reshape((hiddenLayerSize, 10))
+def getCost (x, *args):
+    data, featureSize, hiddenSize, outputSize, reg = args
+    m = np.size(data, 0)
     
-    # 42000 x 1
+    hiddenTheta = x[0 : featureSize * (hiddenSize - 1)].reshape((featureSize, (hiddenSize - 1)))
+    outputTheta = x[featureSize * (hiddenSize - 1) : featureSize * (hiddenSize - 1) + hiddenSize * outputSize].reshape((hiddenSize, outputSize))
+    
+    # m x 1
     labels = data[:, 0]
-    expectedValues = np.eye(10)[labels]
+    expectedValues = np.eye(outputSize)[labels]
     
-    # 42000 x 785
-    features = np.ones((42000, 785))
+    # m x featureSize
+    features = np.ones((m, featureSize))
     features[:, 1:] = data[:, 1:] / 255.0
 
-    # 42000 x 30
-    hiddenLayer = np.ones((42000, hiddenLayerSize))
-    hiddenLayer[:, 1:] = sigmoid(features.dot(hiddenLayerTheta))
+    # m x hiddenSize
+    hiddenLayer = np.ones((m, hiddenSize))
+    hiddenLayer[:, 1:] = sigmoid(features.dot(hiddenTheta))
     
     # 42000 x 10
-    outputLayer = sigmoid(hiddenLayer.dot(outputLayerTheta))
+    outputLayer = sigmoid(hiddenLayer.dot(outputTheta))
     
-    J = -np.sum(expectedValues * np.log(outputLayer + 0.000001) + (1 - expectedValues) * np.log(1 - outputLayer + 0.000001)) / 42000
-    J += reg / (2 * 42000) * (np.sum(hiddenLayerTheta[:, 1:] ** 2) + np.sum(outputLayerTheta[:, 1:] ** 2))
+    J = -np.sum(expectedValues * np.log(outputLayer + 0.000001) + (1 - expectedValues) * np.log(1 - outputLayer + 0.000001)) / m
+    J += reg / (2 * m) * (np.sum(hiddenTheta[:, 1:] ** 2) + np.sum(outputTheta[:, 1:] ** 2))
     return J
 
-def getGradient (x) :
-    # 785 x 29
-    hiddenLayerTheta = x[0 : 785 * (hiddenLayerSize - 1)].reshape((785, (hiddenLayerSize - 1)))
+def getGradient (x, *args) :
+    data, featureSize, hiddenSize, outputSize, reg = args
+    m = np.size(data, 0)
     
-    # 30 x 10
-    outputLayerTheta = x[785 * (hiddenLayerSize - 1) : 785 * (hiddenLayerSize - 1) + hiddenLayerSize * 10].reshape((hiddenLayerSize, 10))
+    hiddenTheta = x[0 : featureSize * (hiddenSize - 1)].reshape((featureSize, (hiddenSize - 1)))
+    outputTheta = x[featureSize * (hiddenSize - 1) : featureSize * (hiddenSize - 1) + hiddenSize * outputSize].reshape((hiddenSize, outputSize))
     
-    # 42000 x 1
+    # m x 1
     labels = data[:, 0]
-    expectedValues = np.eye(10)[labels]
+    expectedValues = np.eye(outputSize)[labels]
     
-    # 42000 x 785
-    features = np.ones((42000, 785))
+    # m x featureSize
+    features = np.ones((m, featureSize))
     features[:, 1:] = data[:, 1:] / 255.0
 
-    # 42000 x 30
-    hiddenLayer = np.ones((42000, hiddenLayerSize))
-    hiddenLayer[:, 1:] = sigmoid(features.dot(hiddenLayerTheta))
+    # m x hiddenSize
+    hiddenLayer = np.ones((m, hiddenSize))
+    hiddenLayer[:, 1:] = sigmoid(features.dot(hiddenTheta))
     
-    # 42000 x 10
-    outputLayer = sigmoid(hiddenLayer.dot(outputLayerTheta))
-    # print outputLayer[0].tolist()
-    # print outputLayer[1].tolist()
+    # m x outputSize
+    outputLayer = sigmoid(hiddenLayer.dot(outputTheta))
+
+    # m x outputSize
+    err3 = (outputLayer - expectedValues) * 1 # sigmoidGrad(hiddenLayer.dot(outputLayerTheta))
     
-    # print "\n"
-    # 42000 x 10
-    err3 = (outputLayer - expectedValues) *1 #sigmoidGrad(hiddenLayer.dot(outputLayerTheta))
+    # m x (hiddenSize - 1)
+    err2 = err3.dot(outputTheta.transpose())[:, 1:] * sigmoidGrad(features.dot(hiddenTheta))
     
-    # 42000 x 29
-    err2 = err3.dot(outputLayerTheta.transpose())[:, 1:] * sigmoidGrad(features.dot(hiddenLayerTheta))
+    hiddenTheta[:, 0] = 0
+    outputTheta[:, 0] = 0
     
-    hiddenLayerTheta[:, 0] = 0
-    outputLayerTheta[:, 0] = 0
+    # hiddenSize x outputSize
+    grad2 = hiddenLayer.transpose().dot(err3) / m + reg * outputTheta / m
     
-    # 30 x 10
-    grad2 = hiddenLayer.transpose().dot(err3) / 42000 + reg * outputLayerTheta / 42000
+    # featureSize x (hiddenSize - 1)
+    grad1 = features.transpose().dot(err2) / m + reg * hiddenTheta / m
     
-    # 785 x 29
-    grad1 = features.transpose().dot(err2) / 42000 + reg * hiddenLayerTheta / 42000
-    # print np.concatenate((grad1.flatten(), grad2.flatten()), axis=0)
     return np.concatenate((grad1.flatten(), grad2.flatten()), axis=0)
 
 
 
 def callback (xk) :
-    global Nfeval
-    print Nfeval
-    Nfeval += 1
+    global iteration
+    print iteration
+    iteration += 1
 
 print "Before learning"
-x0 = np.random.rand(785 * (hiddenLayerSize - 1) + hiddenLayerSize * 10) / 100
-x1 = optimize.fmin_cg(getCost, x0, fprime = getGradient, callback=callback, maxiter=200)
-print predict(x1, 28000)
-np.savetxt("../results.csv", predict(x1, 28000), delimiter=",", fmt="\"%d\"")
+
+x0 = np.random.rand(featureSize * (hiddenSize - 1) + hiddenSize * outputSize) / 100.0
+
+np.savetxt("../results.csv", predict(x0, test, testSize, featureSize, hiddenSize, outputSize), delimiter=",", fmt="\"%d\"")
+
+args = (train, featureSize, hiddenSize, outputSize, reg)
+
+x1 = optimize.fmin_cg(getCost, x0, fprime = getGradient, callback=callback, maxiter=maxIterations, args = args)
+
+np.savetxt("../results.csv", predict(x1, test, testSize, featureSize, hiddenSize, outputSize), delimiter=",", fmt="\"%d\"")
